fa75022401348435f3f50167b89217cf942d8773
Nomthandazoe <44575883+Nomthandazoe@users.noreply.github.com>
2019-09-27 14:14:48 +0200

    Add files via upload

---
+++ scoreKeeper.js
@@ -0,0 +1,54 @@
+var p1Button = document.querySelector("#p1"),
+    p2Button = document.getElementById("p2"),
+    resetButton = document.getElementById("reset"),
+    p1Display = document.querySelector("#p1Display"),
+    p2Display = document.querySelector("#p2Display"),
+    numInput = document.querySelector("input"),
+    winningScoreDisplay = document.querySelector("p span"),
+    p1Score = 0,
+    p2Score = 0,
+    gameOver = false,
+    winningScore = 5;
+
+p1Button.addEventListener("click", function(){
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+    
+});
+
+p2Button.addEventListener("click", function(){
+    if(!gameOver){
+        p2Score++;
+        if(p2Score === winningScore){
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+});
+
+resetButton.addEventListener("click", function(){
+    reset();
+});
+
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
+});
